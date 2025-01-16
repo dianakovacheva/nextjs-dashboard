b9910f9f6b6ad1bf40c6b649b6c3cef3512c3222
Diana Kovacheva <dmarinova@outlook.com>
2025-01-16 13:46:58 +0100

    Add try catch to delete invoice function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -121,8 +121,14 @@ export async function updateInvoice(
 
 // Delete Invoice
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Delete Invoice.",
+    };
+  }
 }
 
 // User Authentication
