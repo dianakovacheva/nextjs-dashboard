16a96e6049973d9061c403093ef16e341eea47e0
Diana Kovacheva <dmarinova@outlook.com>
2025-01-16 14:56:51 +0100

    Fix deleteInvoice function
    Remove return from deleteInvoice function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -125,9 +125,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
   } catch (error) {
-    return {
-      message: "Database Error: Failed to Delete Invoice.",
-    };
+    console.log(error);
   }
 }
 
