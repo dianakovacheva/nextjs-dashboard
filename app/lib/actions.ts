d6355003990d59f319fe974d9a0067972dcbe298
Diana Kovacheva <dmarinova@outlook.com>
2025-01-16 13:41:05 +0100

    Update form action in delete invoice button

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -121,14 +121,8 @@ export async function updateInvoice(
 
 // Delete Invoice
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath("/dashboard/invoices");
-  } catch (error) {
-    return {
-      message: "Database Error: Failed to Delete Invoice.",
-    };
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
 
 // User Authentication
